278fe54119a82799647750c9b05feddd098bc618
Matthew Gonzales <mattgonzales3@yahoo.com>
2024-08-06 17:29:43 -0500

    added icons/button to header

--- fight-me/src/App.js
+++ fight-me/src/App.js
@@ -5,7 +5,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Lets start this shit</h1>
 
       <Header />
       {/* Tinder Cards */}
--- fight-me/src/Header.js
+++ fight-me/src/Header.js
@@ -2,14 +2,20 @@ import React from 'react';
 import "./Header.css";
 import PersonIcon from '@mui/icons-material/Person';
 import ForumIcon from '@mui/icons-material/Forum';
+import SportsMmaIcon from '@mui/icons-material/SportsMma';
+import IconButton from '@mui/material/IconButton';
 
 function Header() {
   return (
     // BEM
     <div className='header'>
-        <PersonIcon />
-        <h2>ima header</h2>
-        <ForumIcon />
+        <IconButton>
+        <PersonIcon className='header__icon' fontSize='large'/>
+        </IconButton>
+        <SportsMmaIcon className='header__icon' fontSize='large'/>
+        <IconButton>
+        <ForumIcon className='header__icon' fontSize='large'/>
+        </IconButton>
     </div>
   )
 }
