5e88f86f862a9d6d13cb0e17fd1b92fc6c7f636b
Matthew Gonzales <mattgonzales3@yahoo.com>
2024-08-06 20:07:57 -0500

    adding fighterCards component/need to run through because screen is blank now

--- fight-me/src/App.js
+++ fight-me/src/App.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import Header from './Header';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route }
+ from "react-router-dom"
+import FighterCards from './FighterCards';
+
 
 function App() {
   return (
     <div className="App">
+    <Header />
+      <Router>
+        <Routes>
+          <Route path='/chat' element={ <h1>ima chat page</h1> }>
 
-      <Header />
-      {/* Tinder Cards */}
-      {/* Buttons below tinder cards */}
+          </Route>
+          <Route path='/' element={ <FighterCards /> }>
 
-      {/* Chat screen */}
-      {/* Individual chat screen */}
+          </Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ fight-me/src/FighterCards.js
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+import TinderCard from "react-tinder-card";
+import "./FighterCards.css";
+
+function FighterCards() {
+    const [people, setPeople] = useState([
+        {
+            name: 'scoob',
+            url: 'https://en.wikipedia.org/wiki/Scooby-Doo_%28character%29#/media/File:Scooby-Doo.png'
+        },
+        {
+            name: 'remy boyz',
+            url: 'https://hero.fandom.com/wiki/Remy_(Ratatouille)?file=Remy.png'
+        }
+    ]);
+
+  return (
+    <div>
+        <h1>Fighter cards</h1>
+
+        {people.map(person => (
+            <TinderCard
+                className="swipe"
+                key={person.name}
+                preventSwipe={['up', 'down']}
+            >
+                <div 
+                style={{ backgroundImage: `url(${person.url})` }}
+                className='card'>
+                    <h3>{person.name}</h3>
+                </div>
+            </TinderCard>
+        ))}
+    </div>
+  )
+}
+
+export default FighterCards
