2147e64047e031f04662cf383a66016c521449d5
Matthew Gonzales <mattgonzales3@yahoo.com>
2024-08-12 21:10:49 -0500

    fix firebase config

--- fight-me/src/firebase.js
+++ fight-me/src/firebase.js
@@ -19,6 +19,6 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-const database = firebaseApp.firestore();
+const database = app.firestore();
 
 export default database;
